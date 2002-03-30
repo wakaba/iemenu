eef2e8b5fd93fc08a9addcf3d1aa087238929d24
Wakaba <w@suika.fam.cx>
2002-03-30 14:57:37 +0000

    2002-03-30  wakaba <w@suika.fam.cx>
    	* ChangeLog: New file.
    	
    	* index.ja.html: New document.
    	* menuitems-add.reg, menuitems-remove.js,
    	user-style-example.css: New files.

---
+++ doc/menuitems-remove.js
@@ -0,0 +1,34 @@
+REGEDIT4
+
+[HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\MenuExt\Show comments]
+@="C:\\windows\\web\\show-comment.html"
+"Contexts"=dword:00000001
+
+[HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\MenuExt\Enable URI tooltip]
+@="C:\\windows\\web\\tip-hrefuri.html"
+"Contexts"=dword:00000020
+
+[HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\MenuExt\Show list of heading]
+@="C:\\windows\\web\\show-heading-list.html"
+"Contexts"=dword:00000001
+
+[HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\MenuExt\Show list of anchors]
+@="C:\\windows\\web\\show-anchor-list.html"
+"Contexts"=dword:00000025
+
+[HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\MenuExt\Show <head> information]
+@="C:\\windows\\web\\show-head.html"
+"Contexts"=dword:00000001
+
+[HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\MenuExt\Show relative URIs]
+@="C:\\windows\\web\\show-relative-uri.html"
+"Contexts"=dword:0000000B
+
+[HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\MenuExt\Open description]
+@="C:\\windows\\web\\open-image-longdesc.html"
+"Contexts"=dword:00000002
+
+[HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\MenuExt\Open description in new window]
+@="C:\\windows\\web\\open-new-image-longdesc.html"
+"Contexts"=dword:00000002
+
